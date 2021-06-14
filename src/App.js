1c3b8e8b09f9193bef0839a4360a5869b9b3d735
GAURAV TYAGI <tyagigaurav.gautam@gmail.com>
2021-06-14 18:19:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { blogData, filterData } from "./utils/commonData";
+
+const App = () => {
+  const [filters, setFilters] = useState([]);
+  const [blogs, setBlogs] = useState([]);
+  const [originalBlogs, setOriginalBlogs] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    setFilters(filterData);
+    setBlogs(blogData);
+    setOriginalBlogs(blogData);
+  }, []);
+
+  const onSearch = () => {
+    const updatedValue = originalBlogs.filter((item) =>
+      item.authorname.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setBlogs(updatedValue);
+  };
+
+  const onReset = () => {
+    setBlogs(originalBlogs);
+  };
+
+  return (
+    <div>
+      <Header />
+
+      <div className="search-div">
+        <input onChange={(e) => setSearchValue(e.target.value)} />
+        <button onClick={onSearch}>Search</button>
+        <button onClick={onReset}>Reset</button>
+      </div>
+
+      <div className="capsule-wrapper">
+        {filters.length &&
+          filters.map(({ id, title }) => (
+            <div className="capsule" key={id}>
+              {title}
+            </div>
+          ))}
+      </div>
+
+      <div className="blog-wrapper">
+        {blogs.length &&
+          blogs.map(
+            ({
+              authorname,
+              id,
+              small_image,
+              posted_on,
+              description,
+              title,
+            }) => (
+              <div className="blog" key={id}>
+                <img src={small_image} alt="img" />
+                <div className="blog-data">
+                  <h4>{title}</h4>
+                  <p className="publish-details">
+                    {authorname}
+                    <span> | {posted_on.slice(0, 10)}</span>
+                  </p>
+                  <p className="blog-desc">{description}</p>
+                </div>
+              </div>
+            )
+          )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="left-navbar">
+        <a href="https://www.edyoda.com">
+          <h3>EDYODA</h3>
+          <p>Stories</p>
+        </a>
+        <a className="categories-href" href="https://www.edyoda.com">
+          Explore Categories
+        </a>
+      </div>
+      <div className="right-navbar">
+        <p>
+          EdYoda is free learning and knowledge <br />
+          sharing platform for techies
+        </p>
+        <a href="https://www.edyoda.com">Go To Main Website</a>
+      </div>
+    </div>
+  );
+};
